b4d5445717a7b7710c135400ba502ba77306c3b9
MonishaAgatheeswaran <43031589+MonishaAgatheeswaran@users.noreply.github.com>
2019-01-10 22:49:56 +0530

    Add files via upload

--- cal.py
+++ cal.py
@@ -1,42 +1,45 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def add(x, y):
-   return x + y
-
-# This function subtracts two numbers 
-def subtract(x, y):
-   return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-   return x * y
-
-# This function divides two numbers
-def divide(x, y):
-   return x / y
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-# Take input from the user 
-choice = input("Enter choice(1/2/3/4):")
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
-
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
-
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
-
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Invalid input")
+def add(num1, num2): 
+    return num1 + num2 
+  
+# Function to subtract two numbers  
+def subtract(num1, num2): 
+    return num1 - num2 
+  
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+    return num1 * num2 
+  
+# Function to divide two numbers 
+def divide(num1, num2): 
+    return num1 / num2 
+  
+print("Please select operation -\n" \ 
+        "1. Add\n" \ 
+        "2. Subtract\n" \ 
+        "3. Multiply\n" \ 
+        "4. Divide\n") 
+  
+  
+# Take input from the user  
+select = input("Select operations form 1, 2, 3, 4 :") 
+  
+number_1 = int(input("Enter first number: ")) 
+number_2 = int(input("Enter second number: ")) 
+  
+if select == '1': 
+    print(number_1, "+", number_2, "=", 
+                    add(number_1, number_2)) 
+  
+elif select == '2': 
+    print(number_1, "-", number_2, "=", 
+                    subtract(number_1, number_2)) 
+  
+elif select == '3': 
+    print(number_1, "*", number_2, "=", 
+                    multiply(number_1, number_2)) 
+  
+elif select == '4': 
+    print(number_1, "/", number_2, "=", 
+                    divide(number_1, number_2)) 
+else: 
+    print("Invalid input") 
